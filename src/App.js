3dc5753b47bc97f8c0d7c8c89bdd277098f850ae
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-10-28 19:05:20 +0100

    added routing and create home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Navbar from "./components/navbar";
-import About from "./components/about";
-import Features from "./components/features";
+import Home from "./pages/home";
 import "./App.css";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <About />
-      <Features />
-      <div className="bottom-wave">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path
-            fill="#FD8061"
-            fill-opacity="1"
-            d="M0,224L120,240C240,256,480,288,720,282.7C960,277,1200,235,1320,213.3L1440,192L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
-          ></path>
-        </svg>
-      </div>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home" component={Home} />
+      </Switch>
     </div>
   );
 };
---
+++ src/pages/home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import About from "../components/about";
+import Features from "../components/features";
+
+const Home = () => {
+  return (
+    <div>
+      <About />
+      <Features />
+      <div className="bottom-wave">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#FD8061"
+            fill-opacity="1"
+            d="M0,224L120,240C240,256,480,288,720,282.7C960,277,1200,235,1320,213.3L1440,192L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
